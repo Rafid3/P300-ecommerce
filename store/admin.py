3a7746d33df8db6fabb68ac82c3c861bcc264ffe
Rafid3 <rafidurrahman97@gmail.com>
2021-03-15 23:55:18 +0600

    first commit

---
+++ store/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import Product, OrderItem, Order, Payment,Address
+
+# Register your models here.
+admin.site.register(Product)
+admin.site.register(OrderItem)
+admin.site.register(Order)
+admin.site.register(Payment)
+admin.site.register(Address)
